85211237e3cd9692faa9c14c4e0af070d48bd927
farhan sharif <farhansharif1410@gmail.com>
2022-08-15 15:52:05 -0230

    list item added and remove work finished

--- listItemAdded/js/script.js
+++ listItemAdded/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello javascript');
+document.getElementById('list-container').addEventListener('click', function (event) {
+    event.target.parentNode.removeChild(event.target)
+})
+
+
+document.getElementById('btn-added').addEventListener('click', function () {
+    const listContainer = document.getElementById('list-container')
+
+    const li = document.createElement('li')
+    li.innerText = 'This is another List tag';
+    li.classList = 'item'
+    listContainer.appendChild(li)
+})
