b521693de4712529eeeb867c7b43662b2a4f8afe
farhan sharif <farhansharif1410@gmail.com>
2022-08-15 15:23:41 -0230

    comment added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello DOm');
+
+document.getElementById('submit-btn').addEventListener('click', function () {
+
+    const textWrite = document.getElementById('text-write');
+    const newText = textWrite.value;
+
+    const commentContainer = document.getElementById('comment-container');
+
+    const p = document.createElement('p')
+
+    p.innerText = newText;
+    commentContainer.appendChild(p)
+
+})
